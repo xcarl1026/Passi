65f09af26723ecfac92fd5727b7f4e8137b67fcd
Carlos Diaz <xcarl1026@gmail.com>
2019-07-10 21:38:30 -0400

    ??? >.>

--- Passi/Pages/Models/DirectoryMethods.cs
+++ Passi/Pages/Models/DirectoryMethods.cs
@@ -68,7 +68,12 @@ namespace Passi.Pages.Models
                     UserPrincipal u = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, de.Properties["samAccountName"].Value.ToString());
                     if (u.Enabled == true)
                     {
-                        ADActiveUserList.Add(u.SamAccountName);
+                        Console.WriteLine(u.SamAccountName.ToLower() + " and " + AppAuth["Username"].ToLower());
+                        if(u.SamAccountName.ToLower() != AppAuth["Username"].ToLower())
+                        {
+                            ADActiveUserList.Add(u.SamAccountName);
+                        }
+                        
                     }
 
                 }
